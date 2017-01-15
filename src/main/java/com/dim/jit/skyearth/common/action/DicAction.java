a1883a14f35a330c957d305d96a0b6b125746d0c
yanming_dai <shuaizhuaidym@sina.com>
2017-01-15 22:28:38 +0800

    add dictionary concept,manage some common data by dictionary

--- src/main/java/com/dim/jit/skyearth/common/action/DicAction.java
+++ src/main/java/com/dim/jit/skyearth/common/action/DicAction.java
@@ -1,15 +1,18 @@
 package com.dim.jit.skyearth.common.action;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping(value="/dic")
 public class DicAction {
 	
 	public String dicForm(){
-		return "dic_form";
+		return "/dic/dic_form";
 	}
 	
+	@RequestMapping(value="/list.action")
 	public String listDic(){
-		return "dic_list";
+		return "/dic/dic_list";
 	}
 }
--- src/main/java/com/dim/jit/skyearth/common/service/DicService.java
+++ src/main/java/com/dim/jit/skyearth/common/service/DicService.java
@@ -1,13 +1,26 @@
 package com.dim.jit.skyearth.common.service;
 
-import java.util.ArrayList;
 import java.util.List;
+
 import com.dim.jit.skyearth.common.Dictionary;
+import com.dim.jit.skyearth.common.dao.IDicDao;
+
 public class DicService {
-	//TODO 缓存处理
-	public List<Dictionary> getDicByType(int type){
-		
-		return new ArrayList<Dictionary>();
+
+	IDicDao dicDao;
+
+	// TODO 缓存处理
+	public List<Dictionary> getDicByType(int type) {
+		List<Dictionary> all = dicDao.findByHql("", null);
+		return all;
 	}
-	
+
+	public IDicDao getDicDao() {
+		return dicDao;
+	}
+
+	public void setDicDao(IDicDao dicDao) {
+		this.dicDao = dicDao;
+	}
+
 }
